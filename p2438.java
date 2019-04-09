6559657c1d2ed4b1c372adfd120f65b5a6368bac
kocucc789 <49758@naver.com>
2019-04-09 21:54:09 +0900

    Create p2438.java
    백준 별세우기-1

---
+++ p2438.java
@@ -0,0 +1,21 @@
+package πÈ¡ÿ;
+
+import java.util.Scanner;
+
+public class p2438 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+				Scanner star=new Scanner(System.in);
+				int n;
+				n=star.nextInt();
+				
+				for(int i=1; i<=n; i++) {    //πÿ¿∏∑Œ ¥√æÓ≥™¥¬ ∫∞
+					for(int j=1; j<=i; j++) { //ø∑¿∏∑Œ ¥√æÓ≥™¥¬ ∫∞∞≥ºˆ
+					     System.out.print("*");
+				}
+					System.out.println("");//¡ŸπŸ≤ﬁ
+				}
+			}
+		}
